2d1d247e26530b5f5a0a361df64ba4b32412c768
Jesse <54597560+Jesse-Yan@users.noreply.github.com>
2025-04-24 12:24:16 -0500

    Update run_evaluation.sh

--- evaluation/run_evaluation.sh
+++ evaluation/run_evaluation.sh
@@ -1,17 +1,17 @@
 # DO NOT CHANGE THIS
-db_root_path='../sqlite/dev_databases/'
-num_cpus=16
+db_root_path='llm/data/dev_databases/'
+num_cpus=32
 meta_time_out=30.0
 # DO NOT CHANGE THIS
 
 # ************************* #
-predicted_sql_path='../sql_result/predict_mini_dev_gpt-4-32k_cot_SQLite.json' # Replace with your predict sql json path
+#predicted_sql_path='../sql_result/predict_mini_dev_gpt-4-32k_cot_SQLite.json' # Replace with your predict sql json path
 # predicted_sql_path='../sql_result/predict_mini_dev_gpt-4-32k_cot_PostgreSQL.json' # Replace with your predict sql json path
-# predicted_sql_path='../sql_result/predict_mini_dev_gpt-4-32k_cot_MySQL.json' # Replace with your predict sql json path
+predicted_sql_path='llm/exp_result/xys_output_kg/predict_mini_dev_XiYanSQL-QwenCoder_cot_MySQL.json' # Replace with your predict sql json path
 
-sql_dialect="SQLite" # ONLY Modify this
+# sql_dialect="SQLite" # ONLY Modify this
 # sql_dialect="PostgreSQL" # ONLY Modify this
-# sql_dialect="MySQL" # ONLY Modify this
+sql_dialect="MySQL" # ONLY Modify this
 # ************************* #
 
 # DO NOT CHANGE THIS
@@ -30,8 +30,8 @@ case $sql_dialect in
     ground_truth_path="../postgresql/mini_dev_postgresql_gold.sql"
     ;;
   "MySQL")
-    diff_json_path="../mysql/mini_dev_mysql.jsonl"
-    ground_truth_path="../mysql/mini_dev_mysql_gold.sql"
+    diff_json_path="llm/data/mini_dev_mysql.json"
+    ground_truth_path="llm/data/mini_dev_mysql_gold.sql"
     ;;
   *)
     echo "Invalid SQL dialect: $sql_dialect"
@@ -48,19 +48,19 @@ echo "Ground Truth Path: $ground_truth_path"
 
 
 echo "starting to compare with knowledge for ex, sql_dialect: ${sql_dialect}"
-python3 -u ./evaluation_ex.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path}  \
+python3 -u evaluation/evaluation_ex.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path}  \
 --ground_truth_path ${ground_truth_path} --num_cpus ${num_cpus} --output_log_path ${output_log_path} \
 --diff_json_path ${diff_json_path} --meta_time_out ${meta_time_out}  --sql_dialect ${sql_dialect}
 
 
 
-# echo "starting to compare with knowledge for R-VES, sql_dialect: ${sql_dialect}"
-# python3 -u ./evaluation_ves.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path}  \
-# --ground_truth_path ${ground_truth_path} --num_cpus ${num_cpus}  --output_log_path ${output_log_path} \
-# --diff_json_path ${diff_json_path} --meta_time_out ${meta_time_out}  --sql_dialect ${sql_dialect}
+echo "starting to compare with knowledge for R-VES, sql_dialect: ${sql_dialect}"
+python3 -u evaluation/evaluation_ves.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path}  \
+--ground_truth_path ${ground_truth_path} --num_cpus ${num_cpus}  --output_log_path ${output_log_path} \
+--diff_json_path ${diff_json_path} --meta_time_out ${meta_time_out}  --sql_dialect ${sql_dialect}
 
 
-# echo "starting to compare with knowledge for soft-f1, sql_dialect: ${sql_dialect}"
-# python3 -u ./evaluation_f1.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path}  \
-# --ground_truth_path ${ground_truth_path} --num_cpus ${num_cpus}  --output_log_path ${output_log_path} \
-# --diff_json_path ${diff_json_path} --meta_time_out ${meta_time_out}   --sql_dialect ${sql_dialect}
+echo "starting to compare with knowledge for soft-f1, sql_dialect: ${sql_dialect}"
+python3 -u evaluation/evaluation_f1.py --db_root_path ${db_root_path} --predicted_sql_path ${predicted_sql_path}  \
+--ground_truth_path ${ground_truth_path} --num_cpus ${num_cpus}  --output_log_path ${output_log_path} \
+--diff_json_path ${diff_json_path} --meta_time_out ${meta_time_out}   --sql_dialect ${sql_dialect}
